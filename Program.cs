6ce895efc32c16870540b6ae6e031fc8b466a7fb
Miracle McMahon <339663114@GoTVDSB.ca>
2025-03-20 14:21:45 -0400

    Finishing part One of the Assignment (it's huge...)

--- Program.cs
+++ Program.cs
@@ -9,19 +9,22 @@ namespace Part_7_Lists_Programming_Assignment
             List<int> numbers = new List<int>();
             Random generator = new Random();
             string choice;
+            int sum = 0, userAdd = 0, userCount, userRemove, countAmount = 0;
+            decimal average = 0M;
             bool done = false;
 
             Console.WriteLine("Hello and Welcome to the List of Integers!");
-            Console.WriteLine("Here's your list of Random Integers:");
-            Console.WriteLine();
-
-            for (int i = 0; i < 25; i++)
-            {
-                numbers.Add(generator.Next(10, 21));
-                Console.Write(numbers[i]);
-            }
+            
             while (!done)
             {
+                Console.WriteLine("Here's your list of Random Integers:");
+                Console.WriteLine();
+
+                for (int i = 0; i < 25; i++)
+                {
+                    numbers.Add(generator.Next(10, 21));
+                    Console.Write(numbers[i]);
+                }
                 Console.WriteLine();
                 Console.WriteLine();
                 Console.WriteLine("What would you like to do with it?");
@@ -47,36 +50,102 @@ namespace Part_7_Lists_Programming_Assignment
                         Console.Write(numbers[i]);
                     }
                     Console.WriteLine("Done!");
+                    Console.WriteLine("Press ENTER to continue:");
+                    Console.ReadLine();
                 }
                 
                 else if (choice == "2" || choice == "make new list")
                 {
-
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        Console.WriteLine();
+                        numbers.Insert(i, generator.Next(10, 21));
+                        Console.Write(numbers[i]);
+                    }
+                    Console.WriteLine("Done!");
+                    Console.WriteLine("Press ENTER to continue:");
+                    Console.ReadLine();
                 }
 
                 else if (choice == "3" || choice == "remove numbers")
                 {
-
+                    Console.WriteLine("Which value from the list (1-25) would you like to get rid of?");
+                    while (!Int32.TryParse(Console.ReadLine(), out userRemove))
+                            Console.WriteLine("Invalid Integer, please try again:");
+                    numbers.Remove(userRemove - 1);
+                    Console.WriteLine("Done!");
+                    Console.WriteLine("Press ENTER to continue:");
+                    Console.ReadLine();
                 }
 
                 else if (choice == "4" || choice == "add numbers")
                 {
-
+                    Console.WriteLine("What number would you like to add to the list?");
+                    while (!Int32.TryParse(Console.ReadLine(), out userAdd));
+                        Console.WriteLine("Invalid Integer, please try again:");
+                    numbers.Add(userAdd);
+                    Console.WriteLine("Done!");
+                    Console.WriteLine("Press ENTER to continue:");
+                    Console.ReadLine();
                 }
 
                 else if (choice == "5" || choice == "count a number")
                 {
-
+                    Console.WriteLine("What number (from 10-20) would you like to count for?");
+                    while (!Int32.TryParse(Console.ReadLine(), out userCount));
+                        Console.WriteLine("Invalid Integer, please try again:");
+                    if (userCount > 20 || userCount < 10)
+                    {
+                        Console.WriteLine("That's not a number within the list, please try again:");
+                        while (!Int32.TryParse(Console.ReadLine(), out userCount)) ;
+                            Console.WriteLine("Invalid Integer, please try again:");
+                    }
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        if (numbers.Contains(userAdd));
+                        {
+                            countAmount++;
+                        }
+                    }
+                    Console.WriteLine($"There was {countAmount} {userCount} in the List!");
+                    Console.WriteLine("Press ENTER to continue:");
+                    Console.ReadLine();
                 }
 
                 else if (choice == "6" || choice == "print large number")
                 {
-
+                    Console.WriteLine(numbers.Max());
+                    Console.WriteLine("Done!");
+                    Console.WriteLine("Press ENTER to continue:");
+                    Console.ReadLine();
                 }
 
                 else if (choice == "7" || choice == "print small number")
                 {
+                    Console.WriteLine(numbers.Min());
+                    Console.WriteLine("Done!");
+                    Console.WriteLine("Press ENTER to continue:");
+                    Console.ReadLine();
+                }
 
+                else if (choice == "8" || choice == "add and average")
+                {
+                    for (int i = 0; i < numbers.Count; i++)
+                    {
+                        sum += numbers[i];
+                    }
+                    average = sum / numbers.Count;
+                    Console.WriteLine($"The Sum of the List would be {sum}");
+                    Console.WriteLine($"And the Average of the List would be {Math.Round(average)}");
+                    Console.WriteLine("Press ENTER to continue:");
+                    Console.ReadLine();
+                }
+
+                else if (choice == "9" || choice == "quit")
+                {
+                    done = true;
+                    Console.WriteLine("Alright, see ya!");
+                    Console.WriteLine();
                 }
             }
             
